4db89a60e3d418338d996ebc750c941daf74de47
RudyToet <rudytoet@live.nl>
2026-06-01 10:02:31 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vpt-v10";
+const CACHE = "vpt-v12";
 const ASSETS = [
   "./", "./index.html", "./manifest.json",
   "./logo.png", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png",
